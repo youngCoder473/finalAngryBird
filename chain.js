50d03803f8848fe00af641088e348ea02c3f1c12
youngCoder473 <lakshya.bordia@gmail.com>
2020-08-01 15:12:33 -0400

    hi

---
+++ chain.js
@@ -0,0 +1,29 @@
+class Chain{
+
+    constructor(bodyA,bodyB){
+        var options={
+            bodyA:bodyA,
+            bodyB:bodyB,
+            length:10,
+            stiffness:0.04
+        }
+
+        this.constraint=Constraint.create(options);
+        World.add(world,this.constraint);
+    }
+
+    display(){
+        push();
+        strokeWeight(10);
+        stroke("red");
+        var posA=this.constraint.bodyA.position
+        var posB=this.constraint.bodyB.position
+        line(posA.x,posA.y,posB.x,posB.y);
+        pop();
+    }
+
+
+
+
+
+}
--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
-
+const Constraint = Matter.Constraint;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
@@ -33,8 +33,10 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    log6 = new Log(100,100,150,PI/2);
 
     bird = new Bird(100,100);
+    chain= new Chain(bird.body,log6.body);
 
 }
 
@@ -58,7 +60,12 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    log6.display();
+
+    
+    
 
     bird.display();
+    chain.display();
     platform.display();
 }
